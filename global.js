6abffcf2216895d4092fce0b40e5293901cdcad0
Surya Setty <siriouslysurya@gmail.com>
2025-04-19 22:32:44 -0700

    lab_3

---
+++ global.js
@@ -0,0 +1,61 @@
+console.log("IT'S ALIVE");
+
+function $$(selector, context = document) {
+    return Array.from(context.querySelectorAll(selector));
+  }
+
+let pages = [
+    { url: '', title: 'Home' },
+    { url: 'projects/', title: 'Projects' },
+    { url: 'contact/', title: 'Contact'},
+    { url: 'cv/', title: 'Resume'},
+  ];    
+
+let nav = document.createElement('nav');
+document.body.prepend(nav);
+
+
+const BASE_PATH = (location.hostname === "localhost" || location.hostname === "127.0.0.1")
+  ? "/"                  // Local server
+  : "/website/";         // GitHub Pages repo name
+
+for (let p of pages) {
+    let url = p.url;
+    let title = p.title;
+    if (!url.startsWith('http')) {
+        url = BASE_PATH + url;
+    }
+    let a = document.createElement('a');
+    a.href = url;
+    a.textContent = title;
+    if (a.host === location.host && a.pathname === location.pathname) {
+        a.classList.add('current');
+      }
+    nav.append(a);
+}
+
+nav.insertAdjacentHTML('beforeend', `<a href=https://github.com/suryasetty05 target = "_blank"> Github </a>`)
+
+document.body.insertAdjacentHTML(
+    'afterbegin',
+    `
+      <label class="color-scheme">
+        Theme:
+        <select id="theme-selector">
+          <option value="light dark">Automatic</option>
+          <option value="light">Light</option>
+          <option value="dark">Dark</option>
+        </select>
+      </label>
+    `
+  );
+  const selector = document.getElementById('theme-selector');
+  if (localStorage.getItem('colorScheme')) {
+    document.documentElement.style.colorScheme = localStorage.getItem('colorScheme');
+    selector.value = localStorage.getItem('colorScheme');
+  }
+  selector.addEventListener('change', function() {
+    document.documentElement.style.colorScheme = selector.value;
+    localStorage.colorScheme =  selector.value;
+    console.log('color scheme changed to', selector.value);
+  });
