169106a5bacf2ab4a1a51ac9713209d6191693f5
Surya Setty <siriouslysurya@gmail.com>
2025-04-26 21:55:02 -0700

    lab4 part 2 and 3
    added latest projects, github async function

--- global.js
+++ global.js
@@ -90,11 +90,20 @@ export async function fetchJSON(url) {
 
 export function renderProjects(project, containerElement, headingLevel = 'h2') {
   containerElement.innerHTML = '';
-  const article = document.createElement('article');
-  article.innerHTML = `
-  <${headingLevel}>${project.title}</${headingLevel}>
-  <img src="${project.image}" alt="${project.title}">
-  <p>${project.description}</p>
-  `;
-  containerElement.appendChild(article);
+
+  project.forEach(project => {
+    const article = document.createElement('article');
+    article.innerHTML = `
+    <${headingLevel}>${project.title}</${headingLevel}>
+    <img src="${project.image}" alt="${project.title}">
+    <p>${project.description}</p>
+    `;
+    containerElement.appendChild(article);
+  });
 }
+
+export async function fetchGitHubData(username) {
+  return fetchJSON(`https://api.github.com/users/${username}`);
+}
+
+const githubData = await fetchGitHubData('suryasetty05');
--- projects/projects.js
+++ projects/projects.js
@@ -4,3 +4,6 @@ const projects = await fetchJSON('../lib/projects.json');
 const projectsContainer = document.querySelector('.projects');
 renderProjects(projects, projectsContainer, 'h3');
 
+const numProjects = projects.length;
+const projectsTitle = document.querySelector('h1')
+projectsTitle.textContent = `${numProjects} Projects!`
