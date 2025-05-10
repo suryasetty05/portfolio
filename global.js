a42c2da84061ccbc1104ca1e2d6366a79969cd45
Surya Setty <siriouslysurya@gmail.com>
2025-05-09 17:19:11 -0700

    formatted style.css

--- global.js
+++ global.js
@@ -9,6 +9,7 @@ let pages = [
     { url: 'projects/', title: 'Projects' },
     { url: 'contact/', title: 'Contact'},
     { url: 'cv/', title: 'Resume'},
+    { url: 'meta/', title: 'Meta'},
   ];    
 
 let nav = document.createElement('nav');
